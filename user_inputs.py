6a2e560bbf6bcbf0a1fb0d6db79dca48c907467a
Facecaat <dimjke7@gmail.com>
2024-10-19 15:04:06 +0300

    Setter/Getter

--- user_inputs.py
+++ user_inputs.py
@@ -1,5 +1,22 @@
+class Tasks:
+
+
+    def __init__(self, tasks):
+        self.tasks = []
+
+
+    @property
+    def task(self):
+        return self.tasks
+
+    @task.setter
+    def task(self, setting):
+        self.tasks.append(setting)
+
+
 def running_app():
     while True:
         command = input("task-cli ")
 
 running_app()
+
