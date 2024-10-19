bec87b0ab0d9a673efd0ddfb3a9788ff65e27513
Facecaat <dimjke7@gmail.com>
2024-10-19 21:57:10 +0300

    Added update function Added exceptions: ID should be a digit ID is not exist

--- user_inputs.py
+++ user_inputs.py
@@ -26,7 +26,7 @@ def running_app():
         if command in ["add", "добавить"]:
             TaskTracker.task_id += 1
             task_value = input()
-            task_dict = {TaskTracker.task_id: task_value}
+            task_dict = {str(TaskTracker.task_id): task_value}
 
             with open('tasks.json', 'r') as file:
                 base_structure = json.load(file)
@@ -39,12 +39,36 @@ def running_app():
             print(f"Task added successfully (ID: {TaskTracker.task_id})")
 
         if command in ["update", "обновить"]:
-            with open("tasks.json") as file:
-                tasks_data_json = json.load(file)
-                print(tasks_data_json)
+            with open('tasks.json', 'r') as file:
+                base_structure = json.load(file)
+
+            id_and_description = input().split(maxsplit=1)
 
 
+            def get_id(id):
+                if not id.isdigit():
+                    raise TypeError("Error: ID should be a digit")
+                return int(id)
+
+            try:
+                task_id = get_id(id_and_description[0])
+                description = id_and_description[1] if len(id_and_description) > 1 else ""
+
+                updatable = False
+                for item in base_structure['NotMarked']:
+                    if str(task_id) in item:
+                        item[str(task_id)] = description
+                        print(f"Task (ID: {task_id}) now is: {description}")
+                        updatable = True
+                        break
+
+                if not updatable:
+                    print("Error: ID is not exist")
 
 
+                with open('tasks.json', 'w') as file:
+                    json.dump(base_structure, file, indent=2)
 
 
+            except TypeError as e:
+                print(e)
