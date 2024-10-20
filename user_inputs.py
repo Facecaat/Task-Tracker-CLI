77181e373599c8a07c45c83524e41cced9acac0e
Facecaat <dimjke7@gmail.com>
2024-10-20 19:40:49 +0300

    Replaced command functions into user_inputs_functions.py Added todo list priorities with "*"

--- user_inputs.py
+++ user_inputs.py
@@ -1,133 +1,41 @@
 import classes
 import user_inputs_functions
 
-#todo 1. обработка переноса из notmarked в finished
-#todo 2. добавить функции вывода определенных списков
-#todo 3. переписать на pydentic
-#todo 3.1. Вставить каждую таску как класс в код
-#todo 3.2. С datetime разобраться как корркетно вставить в инфу о классе
-#todo 4. написать тесты
-#todo 5. эксепшены
-#todo 6. перенести строки обработки команд в файл с функциями
-#todo 7. переписать ввод данных в одну строку на каждой функции
+#todo *  обработка переноса из notmarked в finished
+#todo    добавить функции вывода определенных списков
+#todo ** переписать на pydentic
+#todo ** Вставить каждую таску как класс в код
+#todo ** С datetime разобраться как корркетно вставить в инфу о классе
+#todo *  написать тесты
+#todo *  написать эксепшены
+
+
 
 
 def running_app():
     while True:
-        command: str = input("task-cli ")
-        if command in ["add", "добавить"]:
-            task_value: str = input()
-            #class_task_dick = classes.Task(classes.not_marked_counter, task_value, classes.statuses['1'], )
-            task_dict = {str(classes.not_marked_counter): task_value}
+        command: str = input("task-cli ").split()
+        if command[0] in ["add", "добавить"]:
+            task_value = command[1]
+            user_inputs_functions.add_function(task_value)
+
+        if command[0] in ["update", "обновить"]:
+            id_and_description = command[1], command[2]
+            user_inputs_functions.update_function(id_and_description)
+
+        if command[0] in ["delete", "удалить"]:
+            task_id = command[1]
+            user_inputs_functions.delete_function(task_id)
+
+        if command[0] in ["mark-in-progress", "пометить-на-выполнение", "pmark"]:
+            task_id = command[1]
+            user_inputs_functions.pmark_function(task_id)
+
+        if command[0] in ["mark-done", "поменять-на-окончание", "dmark"]:
+            task_id = command[1]
+            user_inputs_functions.dmark_function(task_id)
+
+
+        if command[0] in ["list", "список"]:
             base_structure = user_inputs_functions.load_json()
-            base_structure['NotMarked'].append(task_dict)
-            user_inputs_functions.dump_json(base_structure)
-            print(f"Task added successfully (ID: {classes.not_marked_counter})")
-            classes.not_marked_counter += 1
-
-        if command in ["update", "обновить"]:
-            base_structure = user_inputs_functions.load_json()
-            id_and_description = input().split(maxsplit=1)
-
-
-
-            try:
-                task_id = user_inputs_functions.get_id(id_and_description[0])
-                description = id_and_description[1] if len(id_and_description) > 1 else ""
-                updatable = False
-                for item in base_structure['NotMarked']:
-                    if str(task_id) in item:
-                        item[str(task_id)] = description
-                        print(f"Task (ID: {task_id}) now is: {description}")
-                        updatable = True
-                        break
-                if not updatable:
-                    print("Error: ID does not exist")
-                user_inputs_functions.dump_json(base_structure)
-
-            except TypeError as e:
-                print(e)
-
-        if command in ["delete", "удалить"]:
-            task_id: str = input()
-
-            try:
-                task_id = user_inputs_functions.get_id(task_id)
-                base_structure = user_inputs_functions.load_json()
-                updatable = False
-                for item in base_structure['NotMarked']:
-                    if str(task_id) in item:
-                        del item[str(task_id)]
-                        print(f"Task deleted successfully (ID: {task_id})")
-                        updatable = True
-                        classes.not_marked_counter -= 1
-                        break
-
-                base_structure['NotMarked'] = [d for d in base_structure['NotMarked'] if d]
-
-                if not updatable:
-                    print("Error: ID does not exist")
-                user_inputs_functions.dump_json(base_structure)
-
-            except TypeError as e:
-                print(e)
-
-
-        if command in ["mark-in-progress", "пометить-на-выполнение", "pmark"]:
-            task_id: str = input()
-
-            try:
-                task_id = user_inputs_functions.get_id(task_id)
-                base_structure = user_inputs_functions.load_json()
-                for item in base_structure['NotMarked']:
-                        if str(task_id) in item:
-                            what_to_add = item[str(task_id)]
-                            updatable = False
-                            base_structure['Marked'].append({str(classes.marked_counter): what_to_add})
-                            del item[str(task_id)]
-                            print(f"ID {classes.marked_counter}: in 'Marked' now")
-                            updatable = True
-                            classes.not_marked_counter -= 1
-                            classes.marked_counter += 1
-                            break
-
-                base_structure['NotMarked'] = [d for d in base_structure['NotMarked'] if d]
-
-
-                if not updatable:
-                    print("Error: ID does not exist")
-
-                user_inputs_functions.dump_json(base_structure)
-
-            except TypeError as e:
-                print(e)
-
-        if command in ["mark-done", "поменять-на-окончание", "dmark"]:
-            task_id: str = input()
-
-            try:
-                task_id = user_inputs_functions.get_id(task_id)
-                base_structure = user_inputs_functions.load_json()
-                for item in base_structure['Marked']:
-                    if str(task_id) in item:
-                        what_to_add = item[str(task_id)]
-                        updatable = False
-                        base_structure['Finished'].append({str(classes.finished_counter): what_to_add})
-                        del item[str(task_id)]
-                        print(f"ID {classes.finished_counter}: in 'Finished' now")
-                        updatable = True
-                        classes.marked_counter -= 1
-                        classes.finished_counter += 1
-                        break
-                    else:
-                        print("Error: ID does not exist in Marked")
-
-                base_structure['Marked'] = [d for d in base_structure['Marked'] if d]
-
-                if not updatable:
-                    print("Error: ID does not exists")
-
-                user_inputs_functions.dump_json(base_structure)
-
-            except TypeError as e:
-                print(e)
+            print(base_structure)
--- user_inputs_functions.py
+++ user_inputs_functions.py
@@ -1,4 +1,5 @@
 import json
+import classes
 import user_inputs
 
 #check_ID_is_digit_function
@@ -17,3 +18,112 @@ def load_json():
 def dump_json(base_structure):
     with open('tasks.json', 'w') as file:
         json.dump(base_structure, file, indent=2)
+
+
+def add_function(task_value):
+    # class_task_dick = classes.Task(classes.not_marked_counter, task_value, classes.statuses['1'], )
+    task_dict = {str(classes.not_marked_counter): task_value}
+    base_structure = load_json()
+    base_structure['NotMarked'].append(task_dict)
+    dump_json(base_structure)
+    print (f"Task added successfully (ID: {classes.not_marked_counter})")
+    classes.not_marked_counter += 1
+
+
+def update_function(id_and_description):
+    try:
+        base_structure = load_json()
+        task_id = get_id(id_and_description[0])
+        description = id_and_description[1] if len(id_and_description) > 1 else ""
+        updatable = False
+        for item in base_structure['NotMarked']:
+            if str(task_id) in item:
+                item[str(task_id)] = description
+                print(f"Task (ID: {task_id}) now is: {description}")
+                updatable = True
+                break
+        if not updatable:
+            print("Error: ID does not exist")
+        dump_json(base_structure)
+
+    except TypeError as e:
+        print(e)
+
+
+def delete_function(task_id):
+    try:
+        task_id = get_id(task_id)
+        base_structure = load_json()
+        updatable = False
+        for item in base_structure['NotMarked']:
+            if str(task_id) in item:
+                del item[str(task_id)]
+                print(f"Task deleted successfully (ID: {task_id})")
+                updatable = True
+                classes.not_marked_counter -= 1
+                break
+
+        base_structure['NotMarked'] = [d for d in base_structure['NotMarked'] if d]
+
+        if not updatable:
+            print("Error: ID does not exist")
+        dump_json(base_structure)
+
+    except TypeError as e:
+        print(e)
+
+
+def pmark_function(task_id):
+    try:
+        task_id = get_id(task_id)
+        base_structure = load_json()
+        for item in base_structure['NotMarked']:
+            if str(task_id) in item:
+                what_to_add = item[str(task_id)]
+                updatable = False
+                base_structure['Marked'].append({str(classes.marked_counter): what_to_add})
+                del item[str(task_id)]
+                print(f"ID {classes.marked_counter}: in 'Marked' now")
+                updatable = True
+                classes.not_marked_counter -= 1
+                classes.marked_counter += 1
+                break
+
+        base_structure['NotMarked'] = [d for d in base_structure['NotMarked'] if d]
+
+        if not updatable:
+            print("Error: ID does not exist")
+
+        dump_json(base_structure)
+
+    except TypeError as e:
+        print(e)
+
+
+def dmark_function(task_id):
+    try:
+        task_id = get_id(task_id)
+        base_structure = load_json()
+        for item in base_structure['Marked']:
+            if str(task_id) in item:
+                what_to_add = item[str(task_id)]
+                updatable = False
+                base_structure['Finished'].append({str(classes.finished_counter): what_to_add})
+                del item[str(task_id)]
+                print(f"ID {classes.finished_counter}: in 'Finished' now")
+                updatable = True
+                classes.marked_counter -= 1
+                classes.finished_counter += 1
+                break
+            else:
+                print("Error: ID does not exist in Marked")
+
+        base_structure['Marked'] = [d for d in base_structure['Marked'] if d]
+
+        if not updatable:
+            print("Error: ID does not exists")
+
+        dump_json(base_structure)
+
+    except TypeError as e:
+        print(e)
